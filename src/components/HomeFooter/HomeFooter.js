6495ea0238dc1c9169d441c2ee718e5d3924518e
matrayu <northridge614@gmail.com>
2019-08-31 18:35:45 -0700

    Updated footer buttons and style

--- src/components/HomeFooter/HomeFooter.js
+++ src/components/HomeFooter/HomeFooter.js
@@ -8,10 +8,8 @@ class HomeFooter extends Component {
                 <div className='HomeFooter bg_img'></div>
                 <div className='get_tickets container'>
                     <h1>Get The Game You Want</h1>
-                    <div className='HomeFooter btn_grp'>
-                        <button>Find Tickets</button>
-                        <button><a href='/login'>Log In</a></button>
-                    </div>
+                    <button>Find Tickets</button>
+                    <button><a href='/login'>Log In</a></button>
                 </div>
             </section>
         )
