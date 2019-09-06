6853648b745cef5e7662f107d78d466728043b24
matrayu <northridge614@gmail.com>
2019-09-06 09:42:19 -0700

    Added basic html and styling to ticket page

--- src/components/Ticket/Ticket.js
+++ src/components/Ticket/Ticket.js
@@ -1,52 +1,56 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
-import './Ticket.css'
+import "./Ticket.css";
 
 class Ticket extends Component {
-    render() {
-        return(
-            <div className='Ticket'>
-                <div className='Ticket__image'></div>
-                <div className='Ticket__data'>
-                    <div className='Ticket__data__container main_text flex fd_col'>
-                        <h2 className='f1'>Lakers vs Houston Rockets</h2>
-                        <div className='Ticket__data__meta_text flex f1 fd_row'>
-                            <p className='f2'>Tuesday Noverber 12, 2019</p>
-                            <p className='f2'>7:30PM Tip-Off</p>
-                        </div>
-                    </div>
-                    <div className='Ticket__data__container pricing flex fd_row'>
-                        <div className='pricing_container flex fd_row f1'>
-                            <p>$150</p>
-                            <div className='fd_col'>
-                                <p>No Fees</p>
-                                <p>No Taxes</p>
-                            </div>
-                        </div>
-                        <div className='pricing_container flex fd_row f1'>
-                            <p>$250</p>
-                            <p>Stubhub</p>
-                        </div>
-                    </div>
-                    <div className='Ticket__data__container quantity flex fd_row'>
-                        <div className='section3 f4'>
-                            <p>Quantity</p>
-                            <select>
-                                <option value='1'>1</option>
-                                <option value='2'>2</option>
-                                <option value='3'>3</option>
-                                <option value='4'>4</option>
-                            </select>
-                        </div>
-                        
-                    </div>
-                    <div className='Ticket__data__container add_to_cart'>
-                        <button>Add To Cart</button>
-                    </div>
-                </div>
+  render() {
+    return (
+      <div className="Ticket">
+        <div className="Ticket__image" />
+        <div className="Ticket__data">
+          <div className="Ticket__data__container main_text flex fd_col">
+            <h2 className="f1">Lakers vs Houston Rockets</h2>
+            <div className="Ticket__data__meta_text flex f1 fd_row">
+              <p className="f2">Tuesday Noverber 12, 2019</p>
+              <p className="f2">7:30PM Tip-Off</p>
             </div>
-        )
-    }
+          </div>
+          <hr />
+          <div className="Ticket__data__container pricing flex fd_row">
+            <div className="pricing_container flex fd_row">
+              <h3>$150</h3>
+              <div className="pricing_container info fd_col">
+                <p>No Fees</p>
+                <p>No Taxes</p>
+              </div>
+            </div>
+            <div className="pricing_container flex fd_row">
+              <h3>$250</h3>
+              <p>Stubhub</p>
+            </div>
+          </div>
+          <hr />
+          <div className="Ticket__data__container quantity flex fd_row">
+            <div className="section3">
+              <div className="sub_section f4">
+                <p>Quantity</p>
+                <select>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                </select>
+              </div>
+            </div>
+          </div>
+          <hr />
+          <div className="Ticket__data__container add_to_cart">
+            <button>Add To Cart</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Ticket;
