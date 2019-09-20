3f68c073f335e1756e61db6bc82487881b343bc3
matrayu <northridge614@gmail.com>
2019-09-19 21:18:00 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import { BrowserRouter } from 'react-router-dom';
 import { TicketListProvider } from './contexts/TicketListContext';
 import { TicketProvider } from './contexts/TicketContext';
 import App from './components/App/App';
-
 import './index.css';
 import 'normalize.css';
 
+require('dotenv').config()
 
 ReactDOM.render(
     <BrowserRouter>
