fdac50a2e2167e5017d614467102bc0134eb71c4
matrayu <northridge614@gmail.com>
2019-10-01 11:28:33 -0700

    Updated config to include the heroku server address

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 module.exports = {
-    API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || "http://localhost:8000/api",
+    //API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || "http://localhost:8000/api",
+    API_ENPOINT: `https://lakeshow.herokuapp.com/`,
     API_KEY: process.env.REACT_APP_API_KEY,
     PP_CID: process.env.REACT_APP_PP_CID,
     PP_CID_PROD: process.env.REACT_APP_PP_CID_PROD,
