53b24de70879e8956c7995f75e6e8d88d3f1efa5
matrayu <northridge614@gmail.com>
2019-10-03 08:43:20 -0700

    Fixed spacing issue on Who We Are

--- src/components/WhoWeAre/WhoWeAre.js
+++ src/components/WhoWeAre/WhoWeAre.js
@@ -6,16 +6,18 @@ class WhoWeAre extends Component {
         return (
             <section className='WhoWeAre'>
                 <div className='WhoWeAre left__container'>
-                    <p id='who_we_are_p'>
-                        <b>First and foremost, we are Lakers fans!</b> 
-                        <br/><br/> 
-                        We have been season ticket holders for years and while we love going to 
-                        every game we can, it’s just not possible. So we’ve decided 
-                        to create a portal and bring our season seats directly to you.
-                        <br/><br/>
-                        Without the middleman marketplace, we can offer our great seats at a deep discount. 
-                        It also gives us an opportunity to build relationships with other Lakers fans! 
-                    </p>
+                    <div className='whoWeAreText_container'>
+                        <p id='who_we_are_p'>
+                            <b>First and foremost, we are Lakers fans!</b> 
+                            <br/><br/> 
+                            We have been season ticket holders for years and while we love going to 
+                            every game we can, it’s just not possible. So we’ve decided 
+                            to create a portal and bring our season seats directly to you.
+                            <br/><br/>
+                            Without the middleman marketplace, we can offer our great seats at a deep discount. 
+                            It also gives us an opportunity to build relationships with other Lakers fans! 
+                        </p>
+                    </div>
                 </div>
                 <div className='WhoWeAre right__container'>
                     <div className='WhoWeAre title__bg'>
