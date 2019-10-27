57b06499c1abd103e612f9187dd0f99eb3dded72
matrayu <northridge614@gmail.com>
2019-10-27 12:53:30 -0700

    Update Seats2.js

--- src/components/Seats/Seats2.js
+++ src/components/Seats/Seats2.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {Image, Transformation, CloudinaryContext} from 'cloudinary-react';
 
 import './Seats2.css'
 
@@ -6,54 +7,56 @@ class Seats extends Component {
     render() {
         return(
             <div className='Seats'>
-                <ul>
-                    <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136803/Lakers/LakeshowTix2020-9_jdoqtz.png" alt="View of the seats before tipoff" loading="lazy"></img></li>
-                    <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136822/Lakers/LakeshowTix2020-8_mzupnh.png" alt="Video of the Lakers court prior to game time" loading="lazy"></img></li>
-                    <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136799/Lakers/LakeshowTix2020-7_x2okxk.png" alt="Seats with one unfolded" loading="lazy"></img></li>
-                    <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136817/Lakers/LakeshowTix2020-6_nz3o8q.png" alt="Seat from the top angle" loading="lazy"></img></li>
-                    <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136821/Lakers/LakeshowTix2020-5_llztjj.png" alt="Seat top unfolded" loading="lazy"></img></li>
-                    <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136883/Lakers/LakeshowTix2020-46_g0auu6.png" alt="Anthony Davis blocks Draymon Greens shot" loading="lazy"></img></li>
-                    <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136822/Lakers/LakeshowTix2020-4_e2r7yq.png" alt="Seat top with the chair folder" loading="lazy"></img></li>
-                    <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136843/Lakers/LakeshowTix2020-47_yumrqv.png" alt="Antetokounmpo drives in for the layup" loading="lazy"></img></li>
-                    <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136843/Lakers/LakeshowTix2020-45_x9wdnj.png" alt="Lebron drives in" loading="lazy"></img></li>
-                    <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136845/Lakers/LakeshowTix2020-44_w32nji.png" alt="Lebron dribbles behind the back" loading="lazy"></img></li>
-                    <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136854/Lakers/LakeshowTix2020-43_jk8ql2.png" alt="Warriors cant get past the Lakers trees inside" loading="lazy"></img></li>
-                    <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136861/Lakers/LakeshowTix2020-42_tdu5lc.png" alt="Lakers focused on defense" loading="lazy"></img></li>
-                    <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136857/Lakers/LakeshowTix2020-41_qhzaoe.png" alt="Lebron drives in for the layup" loading="lazy"></img></li>
-                    <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136868/Lakers/LakeshowTix2020-40_tqv8zd.png" alt="Lebron drives in for the layup again" loading="lazy"></img></li>
-                    <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136828/Lakers/LakeshowTix2020-3_qhn1fe.png" alt="Closeup of seat folded" loading="lazy"></img></li>
-                    <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136863/Lakers/LakeshowTix2020-39_yzcp8l.png" alt="Lebron drives in and goes behind the back" loading="lazy"></img></li>
-                    <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136876/Lakers/LakeshowTix2020-38_a4o31v.png" alt="Lebron drivinf down the lane" loading="lazy"></img></li>
-                    <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136869/Lakers/LakeshowTix2020-37_udjo3f.png" alt="Draymon Green drives in and gets blocked by Anthony Davis" loading="lazy"></img></li>
-                    <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136882/Lakers/LakeshowTix2020-36_zhuco2.png" alt="Warriors inbound the basketball" loading="lazy"></img></li>
-                    <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136881/Lakers/LakeshowTix2020-35_xymtab.png" alt="Cameraman focused on the game" loading="lazy"></img></li>
-                    <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136886/Lakers/LakeshowTix2020-34_zoiy7y.png" alt="Danny Green pulls up for the jumper" loading="lazy"></img></li>
-                    <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136882/Lakers/LakeshowTix2020-33_ub3zbc.png" alt="McGee throws down a dunk" loading="lazy"></img></li>
-                    <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136891/Lakers/LakeshowTix2020-32_eylvml.png" alt="McGee flying in for a monster dunk" loading="lazy"></img></li>
-                    <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136901/Lakers/LakeshowTix2020-31_dchjzh.png" alt="Avery Bradley taking the three from the corner" loading="lazy"></img></li>
-                    <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136891/Lakers/LakeshowTix2020-30_qtblrk.png" alt="Lakers on defense get called for the foul" loading="lazy"></img></li>
-                    <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136829/Lakers/LakeshowTix2020-2_irntio.png" alt="Seats folded and upfolded with the court in the background" loading="lazy"></img></li>
-                    <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136905/Lakers/LakeshowTix2020-29_olas4o.png" alt="Curoso dribbles at center court" loading="lazy"></img></li>
-                    <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136896/Lakers/LakeshowTix2020-28_kgveog.png" alt="McGee grabs the rebound and tries to put it back in" loading="lazy"></img></li>
-                    <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136899/Lakers/LakeshowTix2020-27_fytm0m.png" alt="Curuso drives in for the layup" loading="lazy"></img></li>
-                    <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136906/Lakers/LakeshowTix2020-26_idzvkk.png" alt="Anthony Davis puts up a free throw" loading="lazy"></img></li>
-                    <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136725/Lakers/LakeshowTix2020-25_lsajt7.png" alt="Anthony Davis puts up a free throw again" loading="lazy"></img></li>
-                    <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136902/Lakers/LakeshowTix2020-24_cnfxi1.png" alt="2 cocktails in the making" loading="lazy"></img></li>
-                    <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136743/Lakers/LakeshowTix2020-23_g5zz9r.png" alt="Pregame warmups" loading="lazy"></img></li>
-                    <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136832/Lakers/LakeshowTix2020-1_qj9xob.png" alt="Seats folded from behind with the count in the background" loading="lazy"></img></li>
-                    <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136755/Lakers/LakeshowTix2020-18_kaaydx.png" alt="Side angle of the seats" loading="lazy"></img></li>
-                    <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136764/Lakers/LakeshowTix2020-16_glgrz4.png" alt="Side angle closeup of the seats" loading="lazy"></img></li>
-                    <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136778/Lakers/LakeshowTix2020-15_ugi1au.png" alt="Side angle reversed of the seats" loading="lazy"></img></li>
-                    <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136757/Lakers/LakeshowTix2020-17_q32r5d.png" alt="Another side angle of the seats" loading="lazy"></img></li>
-                    <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136785/Lakers/LakeshowTix2020-12_flhzjg.png" alt="Pregame warmups with an empty STAPLES Center" loading="lazy"></img></li>
-                    <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136817/Lakers/LakeshowTix2020-13_tmix2l.png" alt="Top angle from the side of the seats" loading="lazy"></img></li>
-                    <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136774/Lakers/LakeshowTix2020-14_fk7y4p.png" alt="Side angle looking downward" loading="lazy"></img></li>
-                    <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136776/Lakers/LakeshowTix2020-11_as8bi1.png" alt="Low angle from the cameraman view" loading="lazy"></img></li>
-                    <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136792/Lakers/LakeshowTix2020-10_wydsen.png" alt="Professional camera looking at the court" loading="lazy"></img></li>
-                    <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136859/Lakers/LakeshowTix2020-49_wamllx.png" alt="Wide angle of the court and jumboton showing a Lakers win" loading="lazy"></img></li>
-                    <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136894/Lakers/LakeshowTix2020-48_oyupqv.png" alt="Closeup of jumboton showing the Lakers win" loading="lazy"></img></li>
-                    <li></li>
-                </ul>
+                <CloudinaryContext cloudName='matrayu'>
+                     <ul>
+                        <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136803/Lakers/LakeshowTix2020-9_jdoqtz.png" alt="View of the seats before tipoff" loading="lazy"></img></li>
+                        <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136822/Lakers/LakeshowTix2020-8_mzupnh.png" alt="Video of the Lakers court prior to game time" loading="lazy"></img></li>
+                        <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136799/Lakers/LakeshowTix2020-7_x2okxk.png" alt="Seats with one unfolded" loading="lazy"></img></li>
+                        <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136817/Lakers/LakeshowTix2020-6_nz3o8q.png" alt="Seat from the top angle" loading="lazy"></img></li>
+                        <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136821/Lakers/LakeshowTix2020-5_llztjj.png" alt="Seat top unfolded" loading="lazy"></img></li>
+                        <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136883/Lakers/LakeshowTix2020-46_g0auu6.png" alt="Anthony Davis blocks Draymon Greens shot" loading="lazy"></img></li>
+                        <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136822/Lakers/LakeshowTix2020-4_e2r7yq.png" alt="Seat top with the chair folder" loading="lazy"></img></li>
+                        <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136843/Lakers/LakeshowTix2020-47_yumrqv.png" alt="Antetokounmpo drives in for the layup" loading="lazy"></img></li>
+                        <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136843/Lakers/LakeshowTix2020-45_x9wdnj.png" alt="Lebron drives in" loading="lazy"></img></li>
+                        <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136845/Lakers/LakeshowTix2020-44_w32nji.png" alt="Lebron dribbles behind the back" loading="lazy"></img></li>
+                        <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136854/Lakers/LakeshowTix2020-43_jk8ql2.png" alt="Warriors cant get past the Lakers trees inside" loading="lazy"></img></li>
+                        <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136861/Lakers/LakeshowTix2020-42_tdu5lc.png" alt="Lakers focused on defense" loading="lazy"></img></li>
+                        <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136857/Lakers/LakeshowTix2020-41_qhzaoe.png" alt="Lebron drives in for the layup" loading="lazy"></img></li>
+                        <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136868/Lakers/LakeshowTix2020-40_tqv8zd.png" alt="Lebron drives in for the layup again" loading="lazy"></img></li>
+                        <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136828/Lakers/LakeshowTix2020-3_qhn1fe.png" alt="Closeup of seat folded" loading="lazy"></img></li>
+                        <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136863/Lakers/LakeshowTix2020-39_yzcp8l.png" alt="Lebron drives in and goes behind the back" loading="lazy"></img></li>
+                        <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136876/Lakers/LakeshowTix2020-38_a4o31v.png" alt="Lebron drivinf down the lane" loading="lazy"></img></li>
+                        <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136869/Lakers/LakeshowTix2020-37_udjo3f.png" alt="Draymon Green drives in and gets blocked by Anthony Davis" loading="lazy"></img></li>
+                        <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136882/Lakers/LakeshowTix2020-36_zhuco2.png" alt="Warriors inbound the basketball" loading="lazy"></img></li>
+                        <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136881/Lakers/LakeshowTix2020-35_xymtab.png" alt="Cameraman focused on the game" loading="lazy"></img></li>
+                        <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136886/Lakers/LakeshowTix2020-34_zoiy7y.png" alt="Danny Green pulls up for the jumper" loading="lazy"></img></li>
+                        <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136882/Lakers/LakeshowTix2020-33_ub3zbc.png" alt="McGee throws down a dunk" loading="lazy"></img></li>
+                        <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136891/Lakers/LakeshowTix2020-32_eylvml.png" alt="McGee flying in for a monster dunk" loading="lazy"></img></li>
+                        <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136901/Lakers/LakeshowTix2020-31_dchjzh.png" alt="Avery Bradley taking the three from the corner" loading="lazy"></img></li>
+                        <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136891/Lakers/LakeshowTix2020-30_qtblrk.png" alt="Lakers on defense get called for the foul" loading="lazy"></img></li>
+                        <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136829/Lakers/LakeshowTix2020-2_irntio.png" alt="Seats folded and upfolded with the court in the background" loading="lazy"></img></li>
+                        <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136905/Lakers/LakeshowTix2020-29_olas4o.png" alt="Curoso dribbles at center court" loading="lazy"></img></li>
+                        <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136896/Lakers/LakeshowTix2020-28_kgveog.png" alt="McGee grabs the rebound and tries to put it back in" loading="lazy"></img></li>
+                        <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136899/Lakers/LakeshowTix2020-27_fytm0m.png" alt="Curuso drives in for the layup" loading="lazy"></img></li>
+                        <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136906/Lakers/LakeshowTix2020-26_idzvkk.png" alt="Anthony Davis puts up a free throw" loading="lazy"></img></li>
+                        <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136725/Lakers/LakeshowTix2020-25_lsajt7.png" alt="Anthony Davis puts up a free throw again" loading="lazy"></img></li>
+                        <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136902/Lakers/LakeshowTix2020-24_cnfxi1.png" alt="2 cocktails in the making" loading="lazy"></img></li>
+                        <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136743/Lakers/LakeshowTix2020-23_g5zz9r.png" alt="Pregame warmups" loading="lazy"></img></li>
+                        <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136832/Lakers/LakeshowTix2020-1_qj9xob.png" alt="Seats folded from behind with the count in the background" loading="lazy"></img></li>
+                        <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136755/Lakers/LakeshowTix2020-18_kaaydx.png" alt="Side angle of the seats" loading="lazy"></img></li>
+                        <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136764/Lakers/LakeshowTix2020-16_glgrz4.png" alt="Side angle closeup of the seats" loading="lazy"></img></li>
+                        <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136778/Lakers/LakeshowTix2020-15_ugi1au.png" alt="Side angle reversed of the seats" loading="lazy"></img></li>
+                        <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136757/Lakers/LakeshowTix2020-17_q32r5d.png" alt="Another side angle of the seats" loading="lazy"></img></li>
+                        <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136785/Lakers/LakeshowTix2020-12_flhzjg.png" alt="Pregame warmups with an empty STAPLES Center" loading="lazy"></img></li>
+                        <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136817/Lakers/LakeshowTix2020-13_tmix2l.png" alt="Top angle from the side of the seats" loading="lazy"></img></li>
+                        <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136774/Lakers/LakeshowTix2020-14_fk7y4p.png" alt="Side angle looking downward" loading="lazy"></img></li>
+                        <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136776/Lakers/LakeshowTix2020-11_as8bi1.png" alt="Low angle from the cameraman view" loading="lazy"></img></li>
+                        <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136792/Lakers/LakeshowTix2020-10_wydsen.png" alt="Professional camera looking at the court" loading="lazy"></img></li>
+                        <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136859/Lakers/LakeshowTix2020-49_wamllx.png" alt="Wide angle of the court and jumboton showing a Lakers win" loading="lazy"></img></li>
+                        <li> <img src="https://res.cloudinary.com/matrayu/image/upload/v1572136894/Lakers/LakeshowTix2020-48_oyupqv.png" alt="Closeup of jumboton showing the Lakers win" loading="lazy"></img></li>
+                        <li></li>
+                    </ul>
+                </CloudinaryContext>
             </div>
         )
     }
