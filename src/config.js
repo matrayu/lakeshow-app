a142e346917bc37d66821b1c847dcc47b4bc902d
matrayu <northridge614@gmail.com>
2019-11-09 17:53:53 -0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,8 +1,10 @@
 module.exports = {
-    API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || "http://localhost:8000/api",
+    API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT,
     //API_ENDPOINT: "http://localhost:8000/api",
     API_KEY: process.env.REACT_APP_API_KEY,
     PP_CID: process.env.REACT_APP_PP_CID,
     PP_CID_PROD: process.env.REACT_APP_PP_CID_PROD,
     NODE_ENV: process.env.REACT_APP_NODE_ENV,
+    CLOUDINARY_API_KEY: process.env.REACT_APP_CLOUDINARY_API_KEY,
+    CLOUDINARY_API_SECRET: process.env.REACT_APP_CLOUDINARY_API_SECRET,
 };
