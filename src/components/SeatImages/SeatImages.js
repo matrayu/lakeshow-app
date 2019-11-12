afc23c40e6db4d7cbbbc77c4dad7ea6ed6dc973e
matrayu <northridge614@gmail.com>
2019-11-12 08:04:44 -0800

    Removed filename overlay from images

--- src/components/SeatImages/SeatImages.js
+++ src/components/SeatImages/SeatImages.js
@@ -17,7 +17,7 @@ export default class SeatImages extends Component {
                     //gravity="auto"
                     urlSuffix={urlSuffix}
                 >
-                    <Transformation overlay={{fontFamily: "Arial", fontSize: 45, text: `${image}`}} gravity="south" y="10" background="gray" radius="10" opacity="75" color="white" />
+                    {/* <Transformation overlay={{fontFamily: "Arial", fontSize: 45, text: `${image}`}} gravity="south" y="10" background="gray" radius="10" opacity="75" color="white" /> */}
                     <Transformation fetchFormat="auto" />
                     <Transformation quality="auto" />
                 </Image>
