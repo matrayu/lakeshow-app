64bdf8b5899d6bd519461480ea03aaca707816a1
matrayu <northridge614@gmail.com>
2019-11-23 15:12:57 -0800

    Updated cart to include additional ticket information

--- src/components/CartItem/CartItem.js
+++ src/components/CartItem/CartItem.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import TicketContext from "../../contexts/TicketContext";
 import './CartItem.css'
-
+const moment = require("moment");
 
 export default class CartItem extends React.Component {
     constructor(props) {
@@ -14,10 +14,13 @@ export default class CartItem extends React.Component {
     render(){
         const { product } = this.props
         
+        console.log(product)
         return (
             <div className="CartItem" style={{ marginBottom: "10px"}}>
                 <div className="CartItem-body">
                     <h4 className="CartItem-title text-primary">{`${product.away_team} at ${product.home_team}`}</h4>
+                    <h5 className="CartItem-date"><small></small>{moment(product.local_date, "YYYY-MM-DD").format("dddd, MMMM Do YYYY")}</h5>
+                    <h5 className="CartItem-section"><small></small>Section {product.section} / Row {product.seat_row}</h5>
                     <h5 className="CartItem-text"><small>Price: </small>{product.list_price_ea}</h5>
                     <span className="CartItem-text text-success">
                         <small>Quantity: </small>{product.quantity}</span>
